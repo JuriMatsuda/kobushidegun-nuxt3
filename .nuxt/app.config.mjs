5a6db8c3d5825852122364c44a962a4e459fe69b
Juri Matsuda <matsuda.juri@gmail.com>
2022-11-26 16:50:01 +0900

    yarn install

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/jurimatsuda/Development/javascript/nuxt/kobushidegun-nuxt3/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
